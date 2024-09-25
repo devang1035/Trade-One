97206138ed8d2338d3a378deb61e081aae433660
devang1035 <devangpatel10505@gmail.com>
2024-09-25 11:43:58 +0530

    update timeout v_3

--- Backend/Middlewares/AuthMiddleware.js
+++ Backend/Middlewares/AuthMiddleware.js
@@ -5,15 +5,15 @@ const jwt = require("jsonwebtoken");
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.json({ status: false});
+    return res.status(400).json({ status: false});
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-     return res.json({ status: false })
+     return res.status(401).json({ status: false })
     } else {
       const user = await User.findById(data.id);
       if (user) return res.json({ status: true, user: user.username  })
-      else return res.json({ status: false })
+      else return res.status(402).json({ status: false })
     }
   })
 }
