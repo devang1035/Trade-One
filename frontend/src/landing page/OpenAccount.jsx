3e18f04c8b944e140f647e1aaa6c6cadb3f65156
devang1035 <devangpatel10505@gmail.com>
2024-09-25 13:55:53 +0530

    update openaccount

--- frontend/src/landing page/OpenAccount.jsx
+++ frontend/src/landing page/OpenAccount.jsx
@@ -9,7 +9,7 @@ function OpenAccount() {
 
            <h2 className='mt-5 text-muted'>Open a TradeOne Account</h2> 
            <p className='mt-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-           <Button  variant="contained" style={{width:"20%",margin:"0 auto"}} href="https://https://yourdashboard.netlify.app/">Signup</Button>
+           <Button  variant="contained" style={{width:"20%",margin:"0 auto"}} href="https://yourdashboard.netlify.app/">Signup</Button>
         </div>
     </div>
      );
